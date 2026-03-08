a85443e9ee001797f0d8975b3c132178c9aa4e1f
Gaurav <gmkulkarni7499@gmail.com>
2026-03-08 15:05:13 +0530

    data types

---
+++ GK/bascics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all JS code as newer version 
+
+//alert("gaurav kulkarni")  we are using nodejs , not browser so alert will not work
+
+let name = "gaurav kulkarni"  //string
+let age = 24  //number
+let isLoggedIn = false  //boolean
+let state; //undefined
+
+
+// number => 2 to power 53
+// bigint => greater than 2 to power 53
+// string => ""
+// boolean => true or false
+// null => standalone value that represents nothing
+// undefined => standalone value that represents undefined
+// symbol => unique identifier
+
+// object => collection of key value pairs
+// array => ordered collection of values 
+
+console.log(typeof name) // to check the data type of a variable
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javascript
 javascript test and learning
+for documention use mdn web side 
+and tc39.es for better info 
